a974335b175cdeda9cd80b47cc8fb24dff7c7507
JacksGato <133219034+JacksGato@users.noreply.github.com>
2023-09-01 13:40:50 +0000

    up

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+import dotenv from "dotenv";
 import express from "express";      // Requisição do pacote do express
+dotenv.config();
+import roteadorUsuario from "./routes/usuario.js";
+
+app.use(roteadorUsuario);
+
 const app = express();              // Instancia o Express
-const port = 3000;                  // Define a porta
+const port = 3000;                 // Define a porta
+
+app.use(roteadorUsuario);
+app.use(express.json());
 
 app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   res.json({
-    nome: "Seu_nome_completo",      // Substitua pelo seu nome
+    nome: "Jackson Darley Rocha Santos",      // Substitua pelo seu nome
   });
   console.log("Rota / solicitada");
 });
